24a5b2aa9cb97fcf7f360a9bc1620be00b70209c
Sim1andre <simen@webfordeg.net>
2015-12-12 13:40:30 +0100

    Added favicon

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
   	<meta charset="<?php bloginfo( 'charset' ); ?>" />
   	<meta name="viewport" content="width=device-width" />
   	<title><?php wp_title( ' | ', true, 'right' ); ?></title>
+    <link rel="shortcut icon" href="<?php bloginfo('siteurl'); ?>/favicon.png"/>
   	<?php wp_head(); ?>
 </head>
 <body  <?php body_class(); ?>>
